087cb901c1d0f2b54efabd2a55273fb3de09e31b
Pavel Jorda <pavel.jorda@gmail.com>
2015-07-17 11:15:37 +0200

    Update default.php
    Jsem zapomnel y

--- default.php
+++ default.php
@@ -151,7 +151,7 @@ function getProduct($url) {
         }
     }
     
-    $produkty = array_merge($produkt, getInclusion($url, $obsah_stranky));
+    $produkty = array_merge($produkty, getInclusion($url, $obsah_stranky));
     
     if ($obsah_stranky->find('div.sti_image', 0)) {
       $produkt["url_image"] = 'http://dealer.tsbohemia.cz/'.$obsah_stranky->find('div.sti_image', 0)->find('img', 0)->src;
