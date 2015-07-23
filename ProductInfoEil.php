5c5d35921b86b5d915c61f5d961609adc2d28a4e
MichalDM <jordadm@seznam.cz>
2015-07-23 15:18:24 +0200

    Update ProductInfoEil.php

--- ProductInfoEil.php
+++ ProductInfoEil.php
@@ -22,12 +22,6 @@ class ProductInfoEil {
             $produkt = array();
             $produkt = self::getName($obsah_stranky, $produkt);
             $produkt = self::getInformation($obsah_stranky, $produkt);
-            /*  $produkt = self::getAvailability($obsah_stranky, $produkt);
-              $produkt = self::getPopis($obsah_stranky, $produkt);
-              $produkt = self::getParametr($obsah_stranky, $produkt);
-              $produkt = self::getImage($obsah_stranky, $produkt);
-              $produkt = self::getInclusion($obsah_stranky, $produkt);
-              $produkt = self::unsetInformation($produkt); */
             printr($produkt);
             self::saveProduct($produkt, $soubor);
         }
@@ -65,48 +59,6 @@ class ProductInfoEil {
       return false;
       } */
 
-// Funkce pro přejmenování informací o produktu
- /*   private static function renameInformation($prvek1) {
-        $seznam = array(
-            'Tracklisting' => 'stopy',
-            'Condition' => 'stav',
-            'Availability' => 'dostupnost',
-            'Year of Release' => 'rok',
-            'Artist' => 'umelec',
-            'Title' => 'dilo',
-            'Price' => 'cena',
-            'Format' => 'format',
-            'Record Label' => 'vydavatel',
-            'Country of Origin' => 'zeme_puvodu',
-            'EIL.COM Ref No' => 'kod',
-            'Related Artists' => 'souvisejici_umelci'
-        );
-        $hledej = array_keys($seznam);
-        $nahrad = array_values($seznam);
-        $prvek1 = str_replace(html_entity_decode('&nbsp;'), ' ', $prvek1);
-        $prvek1 = str_replace($hledej, $nahrad, $prvek1);
-        return $prvek1;
-    }*/
-
-// Funkce pro smazání informací o produktu
-    private static function unsetInformation($produkt) {
-        foreach ($produkt as $klic => $value) {
-            if ($klic == 'Dostupnost na pobočkách') {
-                unset($produkt[$klic]);
-            }
-            if ($klic == 'Hodnocení produktu') {
-                unset($produkt[$klic]);
-            }
-            if ($klic == 'Garance ceny') {
-                unset($produkt[$klic]);
-            }
-            if ($klic == 'Mám o tento produkt zájem:') {
-                unset($produkt['parametry'][$klic]);
-            }
-        }
-        return $produkt;
-    }
-
 // Funkce pro smazání přebytečných mezer
     private static function eraseSpace($promenna) {
         $promenna = str_replace(html_entity_decode('&nbsp;'), ' ', $promenna);
@@ -132,12 +84,12 @@ class ProductInfoEil {
         $produkt['stav'] = self::eraseSpace($name);
         return $produkt;
     }
+
     private static function getAvailability($obsah_stranky, $produkt) {
         $name = $obsah_stranky->find('span[itemprop=name]')->plaintext;
         $produkt['dostupnost'] = self::eraseSpace($name);
         return $produkt;
     }
-    
 
     private static function getYear($obsah_stranky, $produkt) {
         $name = $obsah_stranky->find('span[itemprop=name]')->plaintext;
@@ -221,134 +173,6 @@ class ProductInfoEil {
                 }
             }
             $podminka = true;
-        }
-        return $produkt;
-    }
-
-    /*   private static function getInformation($obsah_stranky, $produkt) {
-      $table = $obsah_stranky->find('TABLE BORDER="0"', 1);
-      $radky = $table->find('tr');
-      foreach ($radky as $radek) {
-      $klic = self::findFirst($radek, 'td')->plaintext;
-      $hodnota = $radek->find('td', 1)->plaintext;
-      $produkt['informace'][$klic] = $hodnota;
-      if ($radek->next_sibling() == null) {
-      break;
-      }
-      }
-      return $produkt;
-      } */
-
-// Funkce pro vypsání dostupnosti produktu na jednotlivých prodejnách
-    private static function getAvailability($obsah_stranky, $produkt) {
-        foreach ($obsah_stranky->find('table[class=sti_detail_avail]') as $tabulka) {
-            $i = 0;
-            foreach ($tabulka->find('th') as $dostup) {
-                $pobocka = $dostup->plaintext;
-                if ($tabulka->find('img', $i)) {
-                    $skladem = $tabulka->find('img', $i)->class;
-                } else {
-                    $skladem = '';
-                }
-                if ($skladem == 'img_skladem') {
-                    $skladem = 1;
-                } else {
-                    $skladem = 0;
-                }
-                $i++;
-                $produkt['dostupnost_pobocky'][$pobocka] = $skladem;
-            }
-        }
-        return $produkt;
-    }
-
-// Funkce pro vypsání popisu produktu
-    private static function getPopis($obsah_stranky, $produkt) {
-        $popis = self::findFirst($obsah_stranky, 'div[id=popis-produktu]')->plaintext;
-        $produkt['popis'] = self::eraseSpace($popis);
-        return $produkt;
-    }
-
-// Funkce pro vypsání parametrů produktu
-    private static function getParametr($obsah_stranky, $produkt) {
-        $div = self::findFirst($obsah_stranky, 'div[id=parametry]');
-        foreach ($div->find('table[class=sti_details]') as $tabulka) {
-            $podminka = false;
-            foreach ($tabulka->find('tr') as $parametry) {
-                if ($podminka == true) {
-                    $klic = '';
-                    $hodnota = '';
-                    if (self::findFirst($tabulka, 'td')) {
-                        $klic = self::findFirst($parametry, 'td')->plaintext;
-                        $klic = self::eraseSpace($klic);
-                    }
-                    if ($tabulka->find('td', 1)) {
-                        $hodnota = $parametry->find('td', 1)->plaintext;
-                        $hodnota = self::eraseSpace($hodnota);
-                    }
-                    if ($klic && $hodnota) {
-                        $produkt['parametry'][$klic] = $hodnota;
-                    }
-                }
-                $podminka = true;
-            }
-        }
-        return $produkt;
-    }
-
-// Funkce pro vypsání url obrázků produktu
-    private static function getImage($obsah_stranky, $produkt) {
-
-        $div = self::findFirst($obsah_stranky, 'div.sti_image');
-        $i = 1;
-        if ($div) {
-            $images = $div->find('img');
-            if ($images) {
-                foreach ($images as $image) {
-                    $produkt['gallery_url_image']['Image ' . $i] = self::SERVER_URL .
-                            $image->src;
-                    $i++;
-                }
-            }
-        }
-        $divs = $obsah_stranky->find('div[class=sti_detail_gallery]');
-        foreach ($divs as $div) {
-            $produkt['gallery_url_image']['Image ' . $i] = self::SERVER_URL .
-                    self::findFirst($div, 'img')->src;
-            $i++;
-        }
-        return $produkt;
-    }
-
-// Funkce pro vypsání zařazení produktu do dané kategorie
-    private static function getInclusion($obsah_stranky, $produkt) {
-        foreach ($obsah_stranky->find('div[id=zarazeni-produktu]') as $tabulka) {
-            $podminka = TRUE;
-            $i = 0;
-            $j = 0;
-
-            $zarazeni = self::findFirst($tabulka, 'strong.hcat');
-            while ($podminka && $zarazeni) {
-                if ($zarazeni->next_sibling() != null) {
-                    if ($zarazeni->tag == 'strong') {
-                        $kategorie = $zarazeni->plaintext;
-                        $produkt['zarazeni'][$kategorie] = '';
-                    }
-                    if ($zarazeni->tag == 'a') {
-                        $vetev = self::eraseSpace($zarazeni->plaintext);
-                        if ($produkt['zarazeni'][$kategorie] == '') {
-                            $produkt['zarazeni'][$kategorie] .= $vetev;
-                        } else {
-                            $produkt['zarazeni'][$kategorie] .= ' -> ' . $vetev;
-                        }
-                    }
-                    $zarazeni = $zarazeni->next_sibling();
-                } else {
-                    $podminka = FALSE;
-                }
-                $i++;
-                $j++;
-            }
         }
         return $produkt;
     }
