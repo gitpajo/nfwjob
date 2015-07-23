3ac5b90b3e81f54100189e4ddbe233b1ec095b5b
MichalDM <jordadm@seznam.cz>
2015-07-23 08:58:03 +0200

    Update and rename ProductInfoOil.php to ProductInfoEil.php

---
+++ ProductInfoEil.php
@@ -0,0 +1,11 @@
+    private static function eraseSpace($promenna) {
+        $promenna = str_replace(html_entity_decode('&nbsp;'), ' ', $promenna);
+        $promenna = trim($promenna, ':');
+        return trim($promenna);
+    }
+
+    private static function getName($obsah_stranky, $produkt) {
+        $name = self::findFirst($obsah_stranky, 'title')->plaintext;
+        $produkt['nazev'] = self::eraseSpace($name);
+        return $produkt;
+    }
