d73776fe01107c2918989d159688d543b8b29328
MichalDM <jordadm@seznam.cz>
2015-07-25 22:13:44 +0200

    Update ProductInfo.php

--- ProductInfo.php
+++ ProductInfo.php
@@ -1,2 +1,18 @@
 class ProductInfo {
+
+ private static function eraseSpace($promenna) {
+        $promenna = str_replace(html_entity_decode('&nbsp;'), ' ', $promenna);
+        $promenna = trim($promenna, ':');
+        $promenna = str_replace('&nbsp;', '', $promenna);
+        return trim($promenna);
+    }
+    
+      private static function findFirst($element, $selector) {
+        return $element->find($selector, 0);
+    }
+    
+        private static function saveProduct($produkt, $soubor) {
+        $obsah = print_r($produkt, true);
+        return file_put_contents($soubor, $obsah);
+    }
 }
