fc5e11ccb06e9ba016e3711a1665d9f925e25a63
MichalDM <jordadm@seznam.cz>
2015-07-28 10:24:41 +0200

    Update ProductInfoEil.php

--- ProductInfoEil.php
+++ ProductInfoEil.php
@@ -6,24 +6,26 @@ class ProductInfoEil extends ProductInfo {
 
 // Řídídcí funkce třídy pro výpis jednotlivých produktů a volání privátních funkcí
     static function getProduct($url, $soubor) {
-        $obsah_stranky = file_get_html($url);
-        if ($obsah_stranky == false) {
-            printr('Nelze načíst stránku');
-        } elseif (strpos($url, self::SERVER_URL) == FALSE) {
-            printr('URL neobsahuje doménu ' . $url);
+        if (strpos($url, self::SERVER_URL) === FALSE) {
+            printr('URL neobsahuje doménu ' . self::SERVER_URL);
             $produkt = array('status' => 'Špatná doména ', 'url' => $url);
             self::saveProduct($produkt, $soubor);
-        } elseif (!self::isProduct($obsah_stranky)) {
-            printr('Produkt nenalezen ' . $url);
-            $produkt = array('status' => 'Produkt nenalezen ', 'url' => $url);
-            self::saveProduct($produkt, $soubor);
         } else {
-            $produkt = array();
-            $produkt = self::getName($obsah_stranky, $produkt);
-            $produkt = self::getInformation($obsah_stranky, $produkt);
-            $produkt = self::getImage($obsah_stranky, $produkt);
-            printr($produkt);
-            self::saveProduct($produkt, $soubor);
+            $obsah_stranky = file_get_html($url);
+            if ($obsah_stranky == false) {
+                printr('Nelze načíst stránku');
+            } elseif (!self::isProduct($obsah_stranky)) {
+                printr('Produkt nenalezen ' . $url);
+                $produkt = array('status' => 'Produkt nenalezen ', 'url' => $url);
+                self::saveProduct($produkt, $soubor);
+            } else {
+                $produkt = array();
+                $produkt = self::getName($obsah_stranky, $produkt);
+                $produkt = self::getInformation($obsah_stranky, $produkt);
+                $produkt = self::getImage($obsah_stranky, $produkt);
+                printr($produkt);
+                self::saveProduct($produkt, $soubor);
+            }
         }
     }
 
