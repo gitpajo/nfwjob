36fc8cdbb0e4b1343bbb6f5bc3714e883fb7cae7
MichalDM <jordadm@seznam.cz>
2015-09-05 17:17:19 +0200

    Update ProductInfoBook.php

--- ProductInfoBook.php
+++ ProductInfoBook.php
@@ -1,3 +1,48 @@
 <?php
-      class ProductInfoBook extends ProductInfo {
+
+class ProductInfoBook extends ProductInfo {
+      
+      const SERVER_URL = 'http://katalog-carmen.knihovnabbb.cz/';
+      
+// Řídídcí funkce třídy pro výpis jednotlivých produktů a volání privátních funkcí
+    static function getProduct($url, $soubor) {
+        if (strpos($url, self::SERVER_URL) === FALSE) {
+            printr('URL neobsahuje doménu ' . self::SERVER_URL);
+            $produkt = array('status' => 'Špatná doména ', 'url' => $url);
+            self::saveProduct($produkt, $soubor);
+        } else {
+            $obsah_stranky = file_get_html($url);
+            if ($obsah_stranky == false) {
+                printr('Nelze načíst stránku');
+            } elseif (!self::isProduct($obsah_stranky)) {
+                printr('Produkt nenalezen ' . $url);
+                $produkt = array('status' => 'Produkt nenalezen ', 'url' => $url);
+                self::saveProduct($produkt, $soubor);
+            } else {
+                $produkt = array();
+                $produkt = self::getName($obsah_stranky, $produkt);
+                $produkt = self::getAuthor($obsah_stranky, $produkt);
+                printr($produkt);
+                self::saveProduct($produkt, $soubor);
+            }
+        }
+    }
+    
+    private static function getName($obsah_stranky, $produkt) {
+        $divs = $obsah_stranky->find('div[class=title]');
+        foreach ($divs as $div) {
+            $produkt[]['Title '] = $div->plaintext;
+            $i++;
+        }
+        return $produkt;
+    }
+    
+    private static function getAuthor($obsah_stranky, $produkt) {
+        $divs = $obsah_stranky->find('div[class=author]');
+        foreach ($divs as $div) {
+            $produkt[]['Author '] = $div->plaintext;
+            $i++;
+        }
+        return $produkt;
+    }
 }
